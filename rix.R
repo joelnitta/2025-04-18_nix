25e83172f8235ff7ec7fa479c2ecb051bdefd475
joelnitta <joelnitta@gmail.com>
2025-04-18 08:53:39 +0900

    Initial commit

---
+++ rix.R
@@ -0,0 +1,16 @@
+# Need this for authentication via rix when using gitcreds
+# https://docs.ropensci.org/rix/articles/z-advanced-topic-handling-packages-with-remote-dependencies.html#authenticating-to-github
+my_token <- gitcreds::gitcreds_get()$password
+Sys.setenv(GITHUB_PAT = my_token)
+
+
+# Snapshot rix environment
+rix::rix(
+  r_ver = "4.5.0",
+  r_pkgs = c("glue"),
+  system_pkgs = NULL,
+  git_pkgs = NULL,
+  ide = "positron",
+  project_path = ".",
+  overwrite = TRUE
+)
